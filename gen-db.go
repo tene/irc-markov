5e73cd898a423c84b37d283a2f0e60952be64263
Stephen Weeks <tene@allalone.org>
2014-07-08 00:41:26 -0700

    Normalize input and break into words

--- gen-db.go
+++ gen-db.go
@@ -3,37 +3,64 @@ package main
 import (
 	"bufio"
 	"fmt"
-  "os"
-  "regexp"
-  "strings"
+	"os"
+	"regexp"
+	"strings"
 )
 
-type msg struct {
-  name string
-  text string
+func parseline(line string) (string, string) {
+	_, rest := line[0:5], line[6:]
+	vals := strings.SplitN(rest, ">", 2)
+	name, text := vals[0][2:], vals[1][1:]
+	return name, text
 }
 
-func (m msg) String() string {
-  return fmt.Sprintf("<%s> %s", m.name, m.text)
+func breakwords(m string) {
 }
 
-func parseline(line string) msg {
-  _, rest := line[0:5], line[6:]
-  vals := strings.SplitN(rest, ">", 2)
-  name, text := vals[0][2:], vals[1][1:]
-  return msg{name, text}
+type suffixlist struct {
+	total  int
+	weight map[string]int
 }
 
-func breakwords(m msg) {
+func (sl suffixlist) String() string {
+	return fmt.Sprintf("%v", sl.weight)
+}
+
+func premunge(str string) string {
+	str = strings.ToLower(str)
+	safechars := " "
+	return strings.Map(func(r rune) rune {
+		if strings.IndexRune(safechars, r) >= 0 {
+			return r
+		}
+		if r >= 'a' && r <= 'z' {
+			return r
+		}
+		return -1
+	}, str)
 }
 
 func main() {
-  file, _ := os.Open(os.Args[1])
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    if matched, _ := regexp.Match("^\\d\\d:\\d\\d <", scanner.Bytes()); matched {
-      m := parseline(scanner.Text())
-      fmt.Println(m)
-    }
-  }
+	file, _ := os.Open(os.Args[1])
+	scanner := bufio.NewScanner(file)
+	stats := make(map[string]suffixlist)
+	for scanner.Scan() {
+		if matched, _ := regexp.Match("^\\d\\d:\\d\\d <", scanner.Bytes()); matched {
+			name, text := parseline(scanner.Text())
+			text = premunge(text)
+			words := strings.Split(text, " ")
+			list, ok := stats[name]
+			if !ok {
+				list = suffixlist{0, make(map[string]int)}
+			}
+			for _, word := range words {
+				list.total += 1
+				list.weight[word] += 1
+			}
+			fmt.Println(name, list.total)
+			stats[name] = list
+		}
+	}
+	fmt.Println(stats)
 }
